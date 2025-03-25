725a1366804d9b8e5804f9cd4a68e08a12eb494b
Elisabeth Irgens <hello@elisabethirgens.com>
2025-03-25 16:12:50 +0100

    Add a small dollop of vanilla CSS 🍦

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,7 @@ export default (eleventyConfig) => {
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("d LLL yyyy");
   });
+  eleventyConfig.addPassthroughCopy("css");
   return {
     /* TODO: update below with name of your repo! */
     pathPrefix: "/booster-2025-blog/",
